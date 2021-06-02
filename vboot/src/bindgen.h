caa1bf1d0250f6779dc5a1cf1b4bb425079c733f
Nicholas Bishop <nicholasbishop@google.com>
2021-06-02 16:13:24 -0400

    vboot: clean up includes in bindgen.h
    Change-Id: Ideed2cea214d4bdef5e0447ba72a09a21b6e0f96

--- vboot/src/bindgen.h
+++ vboot/src/bindgen.h
@@ -1,12 +1,6 @@
 #include <stddef.h>
 
-// TODO: clean this list up
-#include <2api.h>
-#include <2nvstorage.h>
 #include <2struct.h>
-#include <2rsa.h>
-#include <2secdata.h>
-#include <load_kernel_fw.h>
 
 const size_t VB2_KEYBLOCK_SIGNATURE_OFFSET =
     offsetof(struct vb2_keyblock, keyblock_signature);
