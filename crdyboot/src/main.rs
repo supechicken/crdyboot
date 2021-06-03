194edbc8425d68b0e17e052b2879b329314469ef
Nicholas Bishop <nicholasbishop@google.com>
2021-06-03 11:46:40 -0400

    crdyboot: proof-of-concept for loading and running the kernel
    For now it's loading from the EFI partition, not the kernel
    partitions, and no verification is done.
    
    Change-Id: I5b9e71429710555465d535b6957e511342d4e6a6

--- crdyboot/src/main.rs
+++ crdyboot/src/main.rs
@@ -2,11 +2,84 @@
 #![no_main]
 #![feature(abi_efiapi)]
 
+extern crate alloc;
+
+use alloc::{vec, vec::Vec};
+use core::convert::TryFrom;
+use log::info;
+use uefi::data_types::chars::NUL_16;
 use uefi::prelude::*;
+use uefi::proto::loaded_image::LoadedImage;
+use uefi::proto::media::file::{
+    File, FileAttribute, FileInfo, FileMode, FileType,
+};
+use uefi::proto::media::fs::SimpleFileSystem;
+use uefi::{Char16, Result};
+
+fn run(image: Handle, bt: &BootServices) -> Result<()> {
+    let sfs = bt.locate_protocol::<SimpleFileSystem>()?;
+    let sfs = sfs.expect("Cannot open `SimpleFileSystem` protocol");
+    let sfs = unsafe { &mut *sfs.get() };
+
+    let mut directory = sfs.open_volume().log_warning()?;
+    let mut f = directory
+        .open(
+            "\\syslinux\\vmlinuz.A",
+            FileMode::Read,
+            FileAttribute::empty(),
+        )
+        .log_warning()?;
+
+    let mut buffer = [0u8; 255];
+    let info = f.get_info::<FileInfo>(&mut buffer).unwrap_success();
+
+    info!("file size: {}", info.file_size());
+
+    let mut v = vec![0; info.file_size() as usize];
+
+    let ft = f.into_type().unwrap_success();
+    if let FileType::Regular(mut f) = ft {
+        f.read(&mut v).unwrap_success();
+    } else {
+        panic!("bad file type");
+    }
+
+    info!("file loaded");
+
+    let image = bt
+        .load_image_from_buffer(image, &v)
+        .expect_success("lifb failed");
+
+    info!("image loaded");
+
+    // TODO: root
+    let load_options_str = "init=/sbin/init boot=local rootwait ro noresume noswap loglevel=7 noinitrd i915.modeset=1 cros_efi cros_debug       root=/dev/sda18";
+    let mut load_options: Vec<Char16> = load_options_str
+        .encode_utf16()
+        .map(|c| Char16::try_from(c).unwrap())
+        .collect();
+    load_options.push(NUL_16);
+
+    let loaded_image =
+        bt.handle_protocol::<LoadedImage>(image).log_warning()?;
+    let loaded_image = unsafe { &mut *loaded_image.get() };
+    loaded_image.set_load_options(
+        load_options.as_ptr(),
+        (2 * load_options.len()) as u32,
+    );
+
+    bt.start_image(image).log_warning()?;
+
+    Status::SUCCESS.into()
+}
 
 #[entry]
-fn efi_main(_image: Handle, st: SystemTable<Boot>) -> Status {
-    uefi_services::init(&st).expect_success("Failed to initialize utilities");
+fn efi_main(image: Handle, st: SystemTable<Boot>) -> Status {
+    uefi_services::init(&st).expect_success("failed to initialize utilities");
+
+    let bt = st.boot_services();
+
+    run(image, bt).expect_success("run failed");
 
     todo!();
 }
