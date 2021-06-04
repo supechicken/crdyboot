87b19d6a4ca5155742f6a322acc7177791a71320
Nicholas Bishop <nicholasbishop@google.com>
2021-06-04 07:22:47 -0400

    vboot: use memoffset crate
    Change-Id: I8edcfec3b57ac0daa17665f4bdbf36a5aabafe78

--- vboot/src/bindgen.h
+++ vboot/src/bindgen.h
@@ -1,10 +1,1 @@
-#include <stddef.h>
-
 #include <2struct.h>
-
-const size_t VB2_KEYBLOCK_SIGNATURE_OFFSET =
-    offsetof(struct vb2_keyblock, keyblock_signature);
-const size_t VB2_KEYBLOCK_HASH_OFFSET =
-    offsetof(struct vb2_keyblock, keyblock_hash);
-const size_t VB2_KEYBLOCK_KEY_OFFSET =
-    offsetof(struct vb2_keyblock, data_key);
