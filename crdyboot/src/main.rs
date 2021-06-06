a0a1c4418fe6010312fb32f77c964158462b5c3a
Nicholas Bishop <nicholasbishop@google.com>
2021-06-06 15:31:24 -0400

    crdyboot: verify, load, and start the kernel
    Not working yet because the kernel image format isn't recognized.
    
    Change-Id: Ia38c89e3373bee478a1144e54d1cd0136082f870

--- crdyboot/src/main.rs
+++ crdyboot/src/main.rs
@@ -12,14 +12,10 @@ use log::info;
 use uefi::data_types::chars::NUL_16;
 use uefi::prelude::*;
 use uefi::proto::loaded_image::LoadedImage;
-use uefi::proto::media::file::{
-    File, FileAttribute, FileInfo, FileMode, FileType,
-};
-use uefi::proto::media::fs::SimpleFileSystem;
-use uefi::proto::media::partition::{
-    GptPartitionEntry, GptPartitionType, PartitionInfo,
-};
+use uefi::proto::media::block::BlockIO;
+use uefi::proto::media::partition::{GptPartitionType, PartitionInfo};
 use uefi::{Char16, Guid, Result};
+use vboot::rimpl::{verify_kernel, PublicKey};
 
 const KERNEL_TYPE_GUID: Guid = Guid::from_values(
     0xfe3a2a5d,
@@ -29,18 +25,21 @@ const KERNEL_TYPE_GUID: Guid = Guid::from_values(
     [0xac, 0xcc, 0x32, 0x85, 0xa3, 0x09],
 );
 
-fn get_kernel_partitions(
-    _image: Handle,
-    bt: &BootServices,
-) -> Result<Vec<GptPartitionEntry>> {
-    info!("partition info");
+// TODO: open protocol vs handle protocol
 
+fn run(crdyboot_image: Handle, bt: &BootServices) -> Result<()> {
     let handles = bt
         .find_handles::<PartitionInfo>()
         .expect_success("Failed to get handles for `PartitionInfo` protocol");
 
-    // We expect to find three: KERN-A, KERN-B, and KERN-C.
-    let mut v = Vec::with_capacity(3);
+    // TODO
+    let test_key_vbpubk =
+        include_bytes!("../../vboot/test_data/kernel_key.vbpubk");
+    let kernel_key = PublicKey::from_le_bytes(test_key_vbpubk).unwrap();
+
+    let a = 1u128;
+    let b = 12742837032881555980u128;
+    info!("BISH: modtest: {}", a % b);
 
     // TODO: use blockio instead just to have less reliance on UEFI
     // implementations working correctly?
@@ -57,70 +56,72 @@ fn get_kernel_partitions(
         if let Some(gpt) = pi.gpt_partition_entry() {
             if { gpt.partition_type_guid } == GptPartitionType(KERNEL_TYPE_GUID)
             {
-                v.push(gpt.clone());
+                // TODO: for now arbitrarily pick the first one found.
+                info!("kernel partition: {:x?}", gpt);
+
+                // Read the whole kernel into memory.
+
+                let bio =
+                    bt.handle_protocol::<BlockIO>(handle).log_warning()?;
+                let bio = unsafe { &*bio.get() };
+
+                info!("got bio: {:?}", bio.media());
+
+                let num_blocks = gpt.ending_lba - gpt.starting_lba + 1;
+                let num_bytes = num_blocks * bio.media().block_size() as u64;
+                info!("num_bytes: {}", num_bytes);
+
+                // TODO: maybe uninit
+                let mut kernel_buffer = vec![0; num_bytes as usize];
+                info!("allocated kernel buffer");
+
+                bio.read_blocks(
+                    bio.media().media_id(),
+                    // This bio is the partition, not the whole
+                    // device, so this is 0 instead of starting_lba.
+                    0,
+                    &mut kernel_buffer,
+                )
+                .log_warning()?;
+
+                info!("done reading blocks");
+
+                // Verifying!
+
+                let kernel_actual =
+                    verify_kernel(&kernel_buffer, &kernel_key).unwrap();
+
+                info!("verified!");
+
+                let kernel_image = bt
+                    .load_image_from_buffer(crdyboot_image, kernel_actual)
+                    .expect_success("lifb failed");
+
+                info!("loaded!");
+
+                // TODO: root
+                let load_options_str = "init=/sbin/init boot=local rootwait ro noresume noswap loglevel=7 noinitrd i915.modeset=1 cros_efi cros_debug       root=/dev/sda18";
+                let mut load_options: Vec<Char16> = load_options_str
+                    .encode_utf16()
+                    .map(|c| Char16::try_from(c).unwrap())
+                    .collect();
+                load_options.push(NUL_16);
+
+                let loaded_image = bt
+                    .handle_protocol::<LoadedImage>(kernel_image)
+                    .log_warning()?;
+                let loaded_image = unsafe { &mut *loaded_image.get() };
+                loaded_image.set_load_options(
+                    load_options.as_ptr(),
+                    (2 * load_options.len()) as u32,
+                );
+
+                info!("starting kernel...");
+
+                bt.start_image(kernel_image).log_warning()?;
             }
         }
     }
-
-    Status::SUCCESS.into_with_val(|| v)
-}
-
-fn run(image: Handle, bt: &BootServices) -> Result<()> {
-    let kernel_partitions = get_kernel_partitions(image, bt).log_warning()?;
-    info!("kernel partitions: {:?}", kernel_partitions);
-
-    let sfs = bt.locate_protocol::<SimpleFileSystem>()?;
-    let sfs = sfs.expect("Cannot open `SimpleFileSystem` protocol");
-    let sfs = unsafe { &mut *sfs.get() };
-
-    let mut directory = sfs.open_volume().log_warning()?;
-    let mut f = directory
-        .open(
-            "\\syslinux\\vmlinuz.A",
-            FileMode::Read,
-            FileAttribute::empty(),
-        )
-        .log_warning()?;
-
-    let mut buffer = [0u8; 255];
-    let info = f.get_info::<FileInfo>(&mut buffer).unwrap_success();
-
-    info!("file size: {}", info.file_size());
-
-    let mut v = vec![0; info.file_size() as usize];
-
-    let ft = f.into_type().unwrap_success();
-    if let FileType::Regular(mut f) = ft {
-        f.read(&mut v).unwrap_success();
-    } else {
-        panic!("bad file type");
-    }
-
-    info!("file loaded");
-
-    let image = bt
-        .load_image_from_buffer(image, &v)
-        .expect_success("lifb failed");
-
-    info!("image loaded");
-
-    // TODO: root
-    let load_options_str = "init=/sbin/init boot=local rootwait ro noresume noswap loglevel=7 noinitrd i915.modeset=1 cros_efi cros_debug       root=/dev/sda18";
-    let mut load_options: Vec<Char16> = load_options_str
-        .encode_utf16()
-        .map(|c| Char16::try_from(c).unwrap())
-        .collect();
-    load_options.push(NUL_16);
-
-    let loaded_image =
-        bt.handle_protocol::<LoadedImage>(image).log_warning()?;
-    let loaded_image = unsafe { &mut *loaded_image.get() };
-    loaded_image.set_load_options(
-        load_options.as_ptr(),
-        (2 * load_options.len()) as u32,
-    );
-
-    bt.start_image(image).log_warning()?;
 
     Status::SUCCESS.into()
 }
