d79a0b9675c4b2c214d14df64658e89edddeab4d
Nicholas Bishop <nicholasbishop@google.com>
2021-06-06 20:00:16 -0400

    crdyboot: refactor PartitionInfo gathering
    Change-Id: Iae2f35220f27328f2b627524f41a493081b9cba8

--- crdyboot/src/main.rs
+++ crdyboot/src/main.rs
@@ -13,7 +13,9 @@ use uefi::data_types::chars::NUL_16;
 use uefi::prelude::*;
 use uefi::proto::loaded_image::LoadedImage;
 use uefi::proto::media::block::BlockIO;
-use uefi::proto::media::partition::{GptPartitionType, PartitionInfo};
+use uefi::proto::media::partition::{
+    GptPartitionEntry, GptPartitionType, PartitionInfo,
+};
 use uefi::{Char16, Guid, Result};
 use vboot::rimpl::{verify_kernel, PublicKey};
 
@@ -27,22 +29,27 @@ const KERNEL_TYPE_GUID: Guid = Guid::from_values(
 
 // TODO: open protocol vs handle protocol
 
-fn run(crdyboot_image: Handle, bt: &BootServices) -> Result<()> {
+struct Partition {
+    handle: Handle,
+    entry: GptPartitionEntry,
+}
+
+// TODO: use blockio instead just to have less reliance on UEFI
+// implementations working correctly?
+//
+// TODO: what happens if there are multiple disks? How do we pick
+// the one we booted from? Presumably there's some way to use the
+// image handle for this.
+fn get_partitions(
+    _crdyboot_image: Handle,
+    bt: &BootServices,
+) -> Result<Vec<Partition>> {
     let handles = bt
         .find_handles::<PartitionInfo>()
         .expect_success("Failed to get handles for `PartitionInfo` protocol");
 
-    // TODO
-    let test_key_vbpubk =
-        include_bytes!("../../vboot/test_data/kernel_key.vbpubk");
-    let kernel_key = PublicKey::from_le_bytes(test_key_vbpubk).unwrap();
+    let mut v = Vec::with_capacity(12);
 
-    // TODO: use blockio instead just to have less reliance on UEFI
-    // implementations working correctly?
-    //
-    // TODO: what happens if there are multiple disks? How do we pick
-    // the one we booted from? Presumably there's some way to use the
-    // image handle for this.
     for handle in handles {
         let pi = bt
             .handle_protocol::<PartitionInfo>(handle)
@@ -50,74 +57,94 @@ fn run(crdyboot_image: Handle, bt: &BootServices) -> Result<()> {
         let pi = unsafe { &*pi.get() };
 
         if let Some(gpt) = pi.gpt_partition_entry() {
-            if { gpt.partition_type_guid } == GptPartitionType(KERNEL_TYPE_GUID)
-            {
-                // TODO: for now arbitrarily pick the first one found.
-                info!("kernel partition: {:x?}", gpt);
+            v.push(Partition {
+                handle,
+                entry: *gpt,
+            });
+        }
+    }
 
-                // Read the whole kernel into memory.
+    Status::SUCCESS.into_with_val(|| v)
+}
 
-                let bio =
-                    bt.handle_protocol::<BlockIO>(handle).log_warning()?;
-                let bio = unsafe { &*bio.get() };
+fn run(crdyboot_image: Handle, bt: &BootServices) -> Result<()> {
+    // TODO
+    let test_key_vbpubk =
+        include_bytes!("../../vboot/test_data/kernel_key.vbpubk");
+    let kernel_key = PublicKey::from_le_bytes(test_key_vbpubk).unwrap();
 
-                info!("got bio: {:?}", bio.media());
+    let partitions = get_partitions(crdyboot_image, bt).log_warning()?;
 
-                let num_blocks = gpt.ending_lba - gpt.starting_lba + 1;
-                let num_bytes = num_blocks * bio.media().block_size() as u64;
-                info!("num_bytes: {}", num_bytes);
+    for partition in partitions {
+        if { partition.entry.partition_type_guid }
+            == GptPartitionType(KERNEL_TYPE_GUID)
+        {
+            // TODO: for now arbitrarily pick the first one found.
+            info!("kernel partition: {:x?}", partition.entry);
 
-                // TODO: maybe uninit
-                let mut kernel_buffer = vec![0; num_bytes as usize];
-                info!("allocated kernel buffer");
+            // Read the whole kernel into memory.
 
-                bio.read_blocks(
-                    bio.media().media_id(),
-                    // This bio is the partition, not the whole
-                    // device, so this is 0 instead of starting_lba.
-                    0,
-                    &mut kernel_buffer,
-                )
+            let bio = bt
+                .handle_protocol::<BlockIO>(partition.handle)
                 .log_warning()?;
+            let bio = unsafe { &*bio.get() };
 
-                info!("done reading blocks");
+            info!("got bio: {:?}", bio.media());
 
-                // Verifying!
+            let num_blocks =
+                partition.entry.ending_lba - partition.entry.starting_lba + 1;
+            let num_bytes = num_blocks * bio.media().block_size() as u64;
+            info!("num_bytes: {}", num_bytes);
 
-                let kernel =
-                    verify_kernel(&kernel_buffer, &kernel_key).unwrap();
+            // TODO: maybe uninit
+            let mut kernel_buffer = vec![0; num_bytes as usize];
+            info!("allocated kernel buffer");
 
-                info!("verified!");
+            bio.read_blocks(
+                bio.media().media_id(),
+                // This bio is the partition, not the whole
+                // device, so this is 0 instead of starting_lba.
+                0,
+                &mut kernel_buffer,
+            )
+            .log_warning()?;
 
-                info!("params: {}", kernel.command_line);
+            info!("done reading blocks");
 
-                let kernel_image = bt
-                    .load_image_from_buffer(crdyboot_image, kernel.data)
-                    .expect_success("lifb failed");
+            // Verifying!
 
-                info!("loaded!");
+            let kernel = verify_kernel(&kernel_buffer, &kernel_key).unwrap();
 
-                // TODO: root
-                let load_options_str = "init=/sbin/init boot=local rootwait ro noresume noswap loglevel=7 noinitrd i915.modeset=1 cros_efi cros_debug       root=/dev/sda18";
-                let mut load_options: Vec<Char16> = load_options_str
-                    .encode_utf16()
-                    .map(|c| Char16::try_from(c).unwrap())
-                    .collect();
-                load_options.push(NUL_16);
+            info!("verified!");
 
-                let loaded_image = bt
-                    .handle_protocol::<LoadedImage>(kernel_image)
-                    .log_warning()?;
-                let loaded_image = unsafe { &mut *loaded_image.get() };
-                loaded_image.set_load_options(
-                    load_options.as_ptr(),
-                    (2 * load_options.len()) as u32,
-                );
+            info!("params: {}", kernel.command_line);
 
-                info!("starting kernel...");
+            let kernel_image = bt
+                .load_image_from_buffer(crdyboot_image, kernel.data)
+                .expect_success("lifb failed");
 
-                bt.start_image(kernel_image).log_warning()?;
-            }
+            info!("loaded!");
+
+            // TODO: root
+            let load_options_str = "init=/sbin/init boot=local rootwait ro noresume noswap loglevel=7 noinitrd i915.modeset=1 cros_efi cros_debug       root=/dev/sda18";
+            let mut load_options: Vec<Char16> = load_options_str
+                .encode_utf16()
+                .map(|c| Char16::try_from(c).unwrap())
+                .collect();
+            load_options.push(NUL_16);
+
+            let loaded_image = bt
+                .handle_protocol::<LoadedImage>(kernel_image)
+                .log_warning()?;
+            let loaded_image = unsafe { &mut *loaded_image.get() };
+            loaded_image.set_load_options(
+                load_options.as_ptr(),
+                (2 * load_options.len()) as u32,
+            );
+
+            info!("starting kernel...");
+
+            bt.start_image(kernel_image).log_warning()?;
         }
     }
 
