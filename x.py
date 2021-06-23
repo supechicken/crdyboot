d0a2e9ac8d93fe801130348e89c001f3f37ce520
Nicholas Bishop <nicholasbishop@google.com>
2021-06-23 17:13:27 -0400

    x.py: make rustc use absolute paths
    Change-Id: I83b358bf51f24e1ee41c7fc26541448f76c59aee

--- x.py
+++ x.py
@@ -8,13 +8,21 @@ import sys
 
 def main():
     repo_dir = os.path.dirname(os.path.realpath(__file__))
-    tools_manifest = os.path.join(repo_dir, 'tools/Cargo.toml')
+    tools_dir = os.path.join(repo_dir, 'tools')
+    tools_manifest = os.path.join(tools_dir, 'Cargo.toml')
 
     cmd = ['cargo', 'run', '--quiet', '--manifest-path', tools_manifest, '--']
     cmd += ['--repo', repo_dir]
     cmd += sys.argv[1:]
 
-    subprocess.run(cmd, check=True)
+    # Make rustc use absolute paths for messages.
+    var_name = 'RUSTFLAGS'
+    env = dict(os.environ)
+    flags = env.get(var_name, '')
+    flags += ' --remap-path-prefix=src={}/src'.format(tools_dir)
+    env[var_name] = flags
+
+    subprocess.run(cmd, check=True, env=env)
 
 
 if __name__ == '__main__':
