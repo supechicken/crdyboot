bc8bf4923d2fd7fe9201bec83349e8fbf7c19c30
Nicholas Bishop <nicholasbishop@google.com>
2021-06-23 10:43:37 -0400

    crdyboot: add a script to run qemu
    Change-Id: I9e21b91bbc5d5dea73088e6e6be3d88fdc987236

---
+++ crdyboot/run_qemu.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# pylint: disable=missing-docstring
+
+import argparse
+import subprocess
+
+
+def run(*cmd):
+    print(' '.join(cmd))
+    subprocess.run(cmd, check=True)
+
+
+def main():
+    """Run crdyboot under QEMU."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ia32', action='store_true',
+                        help='use 32-bit UEFI instead of 64-bit')
+    args = parser.parse_args()
+
+    qemu = 'qemu-system-x86_64'
+    disk = 'volatile/disk.bin'
+
+    if args.ia32:
+        ovmf = 'volatile/ovmf32.fd'
+    else:
+        ovmf = 'volatile/ovmf64.fd'
+
+    # yapf: disable
+    run(qemu,
+        '-enable-kvm',
+        '-m', '1G',
+        '-vga', 'virtio',
+        '-serial', 'stdio',
+        '-nodefaults',
+        '-drive', 'if=pflash,format=raw,readonly,file=' + ovmf,
+        '-drive', 'format=raw,file=' + disk)
+    # yapf: enable
+
+
+if __name__ == '__main__':
+    main()
