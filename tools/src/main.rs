17faed86f899722c38b7b257b371563842bd9f1e
Nicholas Bishop <nicholasbishop@google.com>
2021-06-24 11:09:35 -0400

    tools: refactor qemu into new module
    Change-Id: Iab1d727ffa778b8d696da72d6b0970e1f7d357e6

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,5 +1,6 @@
 mod loopback;
 mod mount;
+mod qemu;
 
 use anyhow::Error;
 use argh::FromArgs;
@@ -9,6 +10,7 @@ use fehler::throws;
 use fs_err as fs;
 use loopback::LoopbackDevice;
 use mount::Mount;
+use qemu::Qemu;
 use std::env;
 
 /// Tools for crdyboot.
@@ -427,41 +429,9 @@ fn run_qemu(opt: &Opt, action: &QemuAction) {
     } else {
         volatile.join("uefi64")
     };
-    let ovmf_code = ovmf_dir.join("OVMF_CODE.fd");
-    let orig_ovmf_vars = ovmf_dir.join("OVMF_VARS.fd");
-    let new_ovmf_vars = ovmf_dir.join("OVMF_VARS.copy.fd");
-    fs::copy(orig_ovmf_vars, &new_ovmf_vars)?;
 
-    let mut cmd = Command::new("qemu-system-x86_64");
-    cmd.add_arg("-enable-kvm");
-    cmd.add_arg("-nodefaults");
-    cmd.add_args(&["-vga", "virtio"]);
-    cmd.add_args(&["-serial", "stdio"]);
-
-    // Give it a small but reasonable amount of memory (the
-    // default of 128M is too small).
-    cmd.add_args(&["-m", "1G"]);
-
-    // These options are needed for SMM as described in
-    // edk2/OvmfPkg/README.
-    cmd.add_args(&["-machine", "q35,smm=on,accel=kvm"]);
-    cmd.add_args(&["-global", "ICH9-LPC.disable_s3=1"]);
-
-    cmd.add_args(&["-global", "driver=cfi.pflash01,property=secure,value=on"]);
-    cmd.add_args(&[
-        "-drive",
-        &format!("if=pflash,format=raw,unit=0,readonly=on,file={}", ovmf_code),
-    ]);
-    cmd.add_args(&[
-        "-drive",
-        &format!(
-            "if=pflash,format=raw,unit=1,readonly=on,file={}",
-            new_ovmf_vars
-        ),
-    ]);
-    cmd.add_args(&["-drive", &format!("format=raw,file={}", disk)]);
-
-    cmd.run()?;
+    let qemu = Qemu::new(&disk, &ovmf_dir);
+    qemu.run()?;
 }
 
 #[throws]
