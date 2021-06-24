c8b6e7979ee0e5b58d624306e08946db3523558d
Nicholas Bishop <nicholasbishop@google.com>
2021-06-24 19:36:17 -0400

    tools: implement shim build
    Change-Id: I6d84d75ffe3143fbacb522e6f6ead88f6b2477fd

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -85,6 +85,13 @@ impl Arch {
     fn all() -> [Arch; 2] {
         [Arch::Ia32, Arch::X64]
     }
+
+    fn as_str(&self) -> &'static str {
+        match self {
+            Arch::Ia32 => "ia32",
+            Arch::X64 => "x64",
+        }
+    }
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -300,7 +307,48 @@ fn run_rustfmt(opt: &Opt) {
 }
 
 #[throws]
+fn build_shim(opt: &Opt) {
+    let shim_dir = opt.volatile_path().join("shim_build");
+    let shim_url = "https://github.com/rhboot/shim.git";
+    let shim_rev = "9f973e4e95b1136b8c98051dbbdb1773072cc998";
+
+    update_local_repo(&shim_dir, shim_url, shim_rev)?;
+
+    let shim_cert = "shim.cer";
+    fs::copy(opt.secure_boot_pub_der(), shim_dir.join(shim_cert))?;
+
+    let arches = [(Arch::X64, "x86_64"), (Arch::Ia32, "ia32")];
+
+    for (arch, shim_arch) in arches {
+        let file_name = format!("shim{}.efi", arch.as_str());
+        let dst_path = opt.volatile_path().join(&file_name);
+        if dst_path.exists() {
+            println!("skipping build: {} already exists", dst_path);
+            continue;
+        }
+
+        let build_dir = shim_dir.join(shim_arch);
+        if !build_dir.exists() {
+            fs::create_dir(&build_dir)?;
+        }
+
+        #[rustfmt::skip]
+        Command::with_args("make", &[
+            "-C", build_dir.as_str(),
+            &format!("ARCH={}", shim_arch),
+            &format!("VENDOR_CERT_FILE=../{}", shim_cert),
+            "TOPDIR=..",
+            "-f", "../Makefile"
+        ]).run()?;
+
+        fs::copy(build_dir.join(&file_name), dst_path)?;
+    }
+}
+
+#[throws]
 fn run_prep_disk(opt: &Opt) {
+    build_shim(opt)?;
+
     let disk = opt.disk_path();
 
     let lo_dev = LoopbackDevice::new(&disk)?;
