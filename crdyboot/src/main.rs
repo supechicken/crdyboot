13f5df89832e71b4a19b689a7d16028fdfcbbdbb
Nicholas Bishop <nicholasbishop@google.com>
2021-07-08 18:42:22 -0400

    crdyboot: only look for kernel partitions on correct disk
    `get_kernel_partitions` now filters the `BlockIO` handles it looks at so that
    only partitions on the same disk as crdyboot itself get looked at.
    
    Change-Id: I83050de6a9404a4d33e625a0926b9aee7388ef53

--- crdyboot/src/main.rs
+++ crdyboot/src/main.rs
@@ -19,6 +19,8 @@ use core::{
 use log::{debug, error, info};
 use uefi::data_types::chars::NUL_16;
 use uefi::prelude::*;
+use uefi::proto::device_path::{DevicePath, DeviceSubType, DeviceType};
+use uefi::proto::loaded_image::LoadedImage;
 use uefi::proto::media::block::BlockIO;
 use uefi::proto::media::partition::{
     GptPartitionEntry, GptPartitionType, PartitionInfo,
@@ -67,26 +69,117 @@ impl fmt::Display for KernelPartition {
     }
 }
 
+/// Open `DevicePath` protocol for `handle`.
+fn device_paths_for_handle(
+    handle: Handle,
+    bt: &BootServices,
+) -> Result<&DevicePath> {
+    let device_path = bt.handle_protocol::<DevicePath>(handle).log_warning()?;
+    let device_path = unsafe { &*device_path.get() };
+    Status::SUCCESS.into_with_val(|| device_path)
+}
+
+/// True if `potential_parent` is the handle representing the disk that
+/// contains the `partition` device.
+///
+/// This is determined by looking at the Device Paths associated with each
+/// handle. The parent device should have exactly the same set of paths, except
+/// that the partition paths end with a Hard Drive Media Device Path.
+fn is_parent_disk(
+    potential_parent: Handle,
+    partition: Handle,
+    bt: &BootServices,
+) -> Result<bool> {
+    let ret = |val: bool| Status::SUCCESS.into_with_val(|| val);
+
+    let potential_parent_paths_iter =
+        device_paths_for_handle(potential_parent, bt)
+            .log_warning()?
+            .iter();
+    let mut partition_paths_iter =
+        device_paths_for_handle(partition, bt).log_warning()?.iter();
+
+    for (parent_path, partition_path) in
+        potential_parent_paths_iter.zip(&mut partition_paths_iter)
+    {
+        if parent_path != partition_path {
+            return ret(false);
+        }
+    }
+
+    // After the zip operation we expect there to be one remaining path for the
+    // partition device; validate that this expectation is met.
+    let final_partition_path = if let Some(path) = partition_paths_iter.next() {
+        path
+    } else {
+        return ret(false);
+    };
+
+    // That final path should be a Hard Drive Media Device Path.
+    if final_partition_path.full_type()
+        != (DeviceType::MEDIA, DeviceSubType::MEDIA_HARD_DRIVE)
+    {
+        return ret(false);
+    }
+
+    ret(true)
+}
+
+/// Search `block_io_handles` for the device that is a parent of
+/// `partition_handle`. See `is_parent_disk` for details.
+fn find_parent_disk(
+    block_io_handles: &[Handle],
+    partition_handle: Handle,
+    bt: &BootServices,
+) -> Result<Option<Handle>> {
+    for handle in block_io_handles {
+        if is_parent_disk(*handle, partition_handle, bt).log_warning()? {
+            return Status::SUCCESS.into_with_val(|| Some(*handle));
+        }
+    }
+
+    Status::SUCCESS.into_with_val(|| None)
+}
+
 // TODO: use blockio instead just to have less reliance on UEFI
 // implementations working correctly?
-//
-// TODO: what happens if there are multiple disks? How do we pick
-// the one we booted from? Presumably there's some way to use the
-// image handle for this.
 fn get_kernel_partitions(
-    _crdyboot_image: Handle,
+    crdyboot_image: Handle,
     bt: &BootServices,
 ) -> Result<Vec<KernelPartition>> {
-    let handles = bt
-        .find_handles::<PartitionInfo>()
-        .expect_success("Failed to get handles for `PartitionInfo` protocol");
+    // Get the LoadedImage protocol for the image handle. This provides a
+    // device handle which should correspond to the disk that the image was
+    // loaded from.
+    let loaded_image = bt
+        .handle_protocol::<LoadedImage>(crdyboot_image)
+        .log_warning()?;
+    let loaded_image = unsafe { &*loaded_image.get() };
+    let partition_handle = loaded_image.device();
 
-    let mut v = Vec::with_capacity(12);
+    // Get all handles that support BlockIO. This includes both disk devices
+    // and logical partition devices.
+    let block_io_handles = bt.find_handles::<BlockIO>().unwrap_success();
 
-    for handle in handles {
-        let pi = bt
-            .handle_protocol::<PartitionInfo>(handle)
-            .expect_success("Failed to get partition info");
+    // Find the parent disk device of the logical partition device.
+    let disk_handle = if let Some(parent) =
+        find_parent_disk(&block_io_handles, partition_handle, bt)
+            .log_warning()?
+    {
+        parent
+    } else {
+        error!("parent disk not found");
+        return Status::NOT_FOUND.into_with_val(Vec::new);
+    };
+
+    let mut v = Vec::with_capacity(2);
+
+    for handle in block_io_handles {
+        // Ignore device handles that aren't a child of the disk.
+        if !is_parent_disk(disk_handle, handle, bt).log_warning()? {
+            continue;
+        }
+
+        let pi = bt.handle_protocol::<PartitionInfo>(handle).log_warning()?;
         let pi = unsafe { &*pi.get() };
 
         if let Some(gpt) = pi.gpt_partition_entry() {
