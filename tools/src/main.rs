090a8561e9e3096bbbe4b788e1ce380ca6716949
Nicholas Bishop <nicholasbishop@google.com>
2021-07-12 09:21:18 -0400

    tools: move ovmf build to new module
    Change-Id: I6e784f6a1bbdede3ca88aa1c1b608cb56571b3c0

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,6 +1,7 @@
 mod gen_disk;
 mod loopback;
 mod mount;
+mod ovmf;
 mod qemu;
 mod shim;
 mod sign;
@@ -289,21 +290,6 @@ fn run_build_enroller(opt: &Opt) {
 }
 
 #[throws]
-fn build_ovmf(arch_flags: &[&str], edk2_dir: &Utf8Path) {
-    // See edk2/OvmfPkg/README for details of these build flags.
-    let mut cmd = Command::new("OvmfPkg/build.sh");
-    cmd.add_args(arch_flags);
-    // Write debug messages to the serial port.
-    cmd.add_args(&["-D", "DEBUG_ON_SERIAL_PORT"]);
-    // Enable secure boot and require SMM. The latter requires a
-    // pflash-backed variable store.
-    cmd.add_args(&["-D", "SECURE_BOOT_ENABLE"]);
-    cmd.add_args(&["-D", "SMM_REQUIRE"]);
-    cmd.set_dir(edk2_dir);
-    cmd.run()?;
-}
-
-#[throws]
 fn copy_file<S, D>(src: S, dst: D)
 where
     S: AsRef<Utf8Path>,
@@ -313,44 +299,6 @@ where
     let dst = dst.as_ref();
     println!("copy {} to {}", src, dst);
     fs::copy(src, dst)?;
-}
-
-#[throws]
-fn run_build_ovmf(opt: &Opt) {
-    let edk2_dir = opt.volatile_path().join("edk2");
-    let edk2_url = "https://github.com/tianocore/edk2.git";
-    // Known-working commit.
-    let edk2_rev = "75e9154f818a58ffc3a28db9f8c97279e723f02d";
-
-    update_local_repo(&edk2_dir, edk2_url, edk2_rev)?;
-
-    let arch_flags = [
-        // 64-bit UEFI for a 64-bit CPU.
-        vec!["-a", "X64"],
-        // 32-bit UEFI for a 64-bit CPU.
-        vec!["-a", "IA32", "-a", "X64"],
-    ];
-
-    for arf in arch_flags {
-        build_ovmf(&arf, &edk2_dir)?;
-    }
-
-    // Copy the outputs to a more convenient location.
-    let compiler = "DEBUG_GCC5";
-    let outputs = [("Ovmf3264", Arch::Ia32), ("OvmfX64", Arch::X64)];
-    for (src_name, arch) in outputs {
-        let src_dir = edk2_dir.join("Build").join(src_name).join(compiler);
-        let fv_dir = src_dir.join("FV");
-        let efi_dir = src_dir.join("X64");
-
-        let dst = opt.ovmf_paths(arch);
-        copy_file(fv_dir.join("OVMF_CODE.fd"), dst.code())?;
-        copy_file(fv_dir.join("OVMF_VARS.fd"), dst.original_vars())?;
-        copy_file(
-            efi_dir.join("EnrollDefaultKeys.efi"),
-            dst.enroll_executable(),
-        )?;
-    }
 }
 
 #[throws]
@@ -492,7 +440,7 @@ fn main() {
     match &opt.action {
         Action::Build(_) => run_crdyboot_build(&opt),
         Action::BuildEnroller(_) => run_build_enroller(&opt),
-        Action::BuildOvmf(_) => run_build_ovmf(&opt),
+        Action::BuildOvmf(_) => ovmf::run_build_ovmf(&opt),
         Action::Check(_) => run_check(&opt),
         Action::Format(_) => run_rustfmt(&opt),
         Action::UpdateDisk(_) => run_update_disk(&opt),
