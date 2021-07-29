92a1cb0123c3531e69749bff0ab9977248a8e9b2
Nicholas Bishop <nicholasbishop@google.com>
2021-07-29 13:46:16 -0400

    vboot: use vboot_reference to load and verify the kernel
    Change-Id: Ic039c254dbb9e3008e79c0e6a62f75251c29633c

--- vboot/src/lib.rs
+++ vboot/src/lib.rs
@@ -1,23 +1,43 @@
-#![no_std]
+// Turn off std, except when running tests.
+#![cfg_attr(not(test), no_std)]
+#![feature(c_variadic)]
 
 extern crate alloc;
 
+mod disk;
 mod gpt;
 mod kernel;
+mod load_kernel;
+mod printf;
+mod stubs;
+
+#[cfg(not(target_env = "gnu"))]
+mod malloc;
 
 #[allow(clippy::missing_safety_doc)]
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
 pub mod vboot_sys {
+    // The signedness of vb2_return_code created by bindgen differs
+    // depending on the target
+    #[cfg(target_arch = "x86")]
+    type vb2_error_t = u32;
+    #[cfg(target_arch = "x86_64")]
+    type vb2_error_t = i32;
+
     include!(concat!(env!("OUT_DIR"), "/vboot_bindgen.rs"));
 
     // ctypes
     type c_char = i8;
     type c_int = i32;
+    type c_void = core::ffi::c_void;
 }
 
+pub use disk::DiskIo;
 pub use gpt::CgptAttributes;
 pub use kernel::{verify_kernel, PublicKey};
+pub use load_kernel::{load_kernel, LoadedKernel};
+pub use vboot_sys::vb2_return_code as return_code;
 
 /// Get an &T backed by a byte slice. The slice is checked to make sure it's
 /// at least as large as the size of T.
