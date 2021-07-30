8683f5b2c74f9921197ddfbb07b171460b93262c
Nicholas Bishop <nicholasbishop@google.com>
2021-07-30 13:38:58 -0400

    crdyboot: fully implement Display for Error
    Change-Id: I5383b032185a8d9cf2fdcd2504ae8c67611cd759

--- crdyboot/src/result.rs
+++ crdyboot/src/result.rs
@@ -1,21 +1,18 @@
 use core::fmt;
 use uefi::Status;
 
-#[derive(Debug)]
 pub enum Error {
     UefiServicesInitFailed(Status),
 
     GetCommandLineFailed,
-
     CommandLineIsNotAscii,
 
+    BlockIoProtocolMissing(Status),
     DevicePathProtocolMissing(Status),
     LoadedImageProtocolMissing(Status),
-    BlockIoProtocolMissing(Status),
 
     ParentDiskNotFound,
 
-    // TODO: think about how to print better errors
     LoadKernelFailed(vboot::return_code),
 
     GetPeEntryPointFailed,
@@ -31,12 +28,62 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use Error::*;
 
+        let mut write_with_status =
+            |msg, status| write!(f, "{}: {:?}", msg, status);
+
         match self {
             UefiServicesInitFailed(status) => {
-                write!(f, "failed to initialize UEFI services: {:?}", status)
+                write_with_status("failed to initialize UEFI services", status)
             }
-            // TODO
-            err => write!(f, "error: {:?}", err),
+
+            GetCommandLineFailed => {
+                write!(f, "failed to get kernel command line")
+            }
+            CommandLineIsNotAscii => {
+                write!(f, "failed to convert kernel command line to UCS-2")
+            }
+
+            BlockIoProtocolMissing(status) => {
+                write_with_status("failed to get UEFI BlockIO protocol", status)
+            }
+            DevicePathProtocolMissing(status) => write_with_status(
+                "failed to get UEFI DevicePath protocol",
+                status,
+            ),
+            LoadedImageProtocolMissing(status) => write_with_status(
+                "failed to get UEFI LoadedImage protocol",
+                status,
+            ),
+
+            ParentDiskNotFound => {
+                write!(f, "failed to get parent disk")
+            }
+
+            LoadKernelFailed(code) => {
+                // TODO: print human-readable error names.
+                write!(f, "failed to load kernel: {:x}", code.0)
+            }
+
+            GetPeEntryPointFailed => {
+                write!(f, "failed to get PE entry point")
+            }
+
+            KernelTooSmall => {
+                write!(f, "kernel data is too small to contain boot parameters")
+            }
+            InvalidBootParameters => {
+                write!(f, "invalid boot parameters")
+            }
+            KernelDataTooBig(size) => {
+                write!(f, "kernel data is too large: {}", size)
+            }
+            CommandLineTooBig(size) => {
+                write!(f, "kernel command line is too large: {}", size)
+            }
+
+            KernelDidNotTakeControl => {
+                write!(f, "failed to transfer control to the kernel")
+            }
         }
     }
 }
