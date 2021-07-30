f25044295ef4f300a7be8c1845277f49b5e58fae
Nicholas Bishop <nicholasbishop@google.com>
2021-07-30 18:41:11 -0400

    vboot: clean up and comment the C code
    Change-Id: Ie55cfebed305dbbcaae50945aefd26665f067dca

--- vboot/src/bridge.c
+++ vboot/src/bridge.c
@@ -1,19 +1,15 @@
 #include "bindgen.h"
 
-// TODO: clean up
-#include "2api.h"
-#include "2common.h"
-#include "2secdata.h"
-#include "2sysincludes.h"
-#include "vboot_api.h"
-
+// Update the shared data struct to point at the packed kernel key. Also
+// take care of calling vb2_set_workbuf_used.
+//
+// This code is written in C instead of Rust since it calls macros and
+// inline functions.
 void crdyboot_set_kernel_key(struct vb2_context *ctx,
                              const struct vb2_packed_key *packed_key,
                              const struct vb2_workbuf *wb) {
   struct vb2_shared_data *sd = vb2_get_sd(ctx);
   sd->kernel_key_offset = vb2_offset_of(sd, packed_key);
   sd->kernel_key_size = packed_key->key_offset + packed_key->key_size;
-
-  // TODO
   vb2_set_workbuf_used(ctx, vb2_offset_of(sd, wb->buf));
 }
