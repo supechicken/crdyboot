d68e106ae6d9045a51572168875780ae7fef7c2c
Nicholas Bishop <nicholasbishop@google.com>
2021-08-03 14:51:03 -0400

    Move kernel structs from crdyboot to vboot
    Change-Id: I024a7ad28afa59ed5e127af53aeb7220b5b12d5e

--- crdyboot/src/handover.rs
+++ crdyboot/src/handover.rs
@@ -12,88 +12,7 @@ use core::convert::TryInto;
 use core::{mem, ptr};
 use uefi::table::{Boot, SystemTable};
 use uefi::Handle;
-use vboot::struct_from_bytes;
-
-#[repr(C, packed)]
-struct setup_header {
-    setup_sects: u8,
-    root_flags: u16,
-    syssize: u32,
-    ram_size: u16,
-    vid_mode: u16,
-    root_dev: u16,
-    boot_flag: u16,
-    jump: u16,
-    header: u32,
-    version: u16,
-    realmode_swtch: u32,
-    start_sys_seg: u16,
-    kernel_version: u16,
-    type_of_loader: u8,
-    loadflags: u8,
-    setup_move_size: u16,
-    code32_start: u32,
-    ramdisk_image: u32,
-    ramdisk_size: u32,
-    bootsect_kludge: u32,
-    heap_end_ptr: u16,
-    ext_loader_ver: u8,
-    ext_loader_type: u8,
-    cmd_line_ptr: u32,
-    initrd_addr_max: u32,
-    kernel_alignment: u32,
-    relocatable_kernel: u8,
-    min_alignment: u8,
-    xloadflags: u16,
-    cmdline_size: u32,
-    hardware_subarch: u32,
-    hardware_subarch_data: u64,
-    payload_offset: u32,
-    payload_length: u32,
-    setup_data: u64,
-    pref_address: u64,
-    init_size: u32,
-    handover_offset: u32,
-}
-
-#[repr(C, packed)]
-struct boot_params {
-    screen_info: [u8; 64],
-    apm_bios_info: [u8; 20],
-    _pad2: [u8; 4],
-    tboot_addr: u64,
-    ist_info: [u8; 16],
-    _pad3: [u8; 16],
-    hd0_info: [u8; 16],
-    hd1_info: [u8; 16],
-    sys_desc_table: [u8; 16],
-    olpc_ofw_header: [u8; 16],
-    ext_ramdisk_image: u32,
-    ext_ramdisk_size: u32,
-    ext_cmd_line_ptr: u32,
-    _pad4: [u8; 116],
-    edid_info: [u8; 128],
-    efi_info: [u8; 32],
-    alt_mem_k: u32,
-    scratch: u32,
-    e820_entries: u8,
-    eddbuf_entries: u8,
-    edd_mbr_sig_buf_entries: u8,
-    kbd_status: u8,
-    secure_boot: u8,
-    _pad5: [u8; 2],
-    sentinel: u8,
-    _pad6: [u8; 1],
-    hdr: setup_header,
-    _pad7: [u8; 40],
-    edd_mbr_sig_buffer: [u32; 16],
-    e820_table: [u8; 2560],
-    _pad8: [u8; 48],
-    eddbuf: [u8; 492],
-    _pad9: [u8; 276],
-}
-
-const SETUP_MAGIC: u32 = 0x53726448; // "HdrS"
+use vboot::{struct_from_bytes, BootParams, SETUP_MAGIC};
 
 /// Convert the slice to a pointer, then attempt to convert the pointer to a
 /// `u32`. Return `Error::BadNumericConversion` on failure.
@@ -116,7 +35,7 @@ pub fn execute_linux_kernel_32(
     cmdline: &str,
 ) -> Result<()> {
     let image_params = if let Some(image_params) =
-        unsafe { struct_from_bytes::<boot_params>(kernel_data) }
+        unsafe { struct_from_bytes::<BootParams>(kernel_data) }
     {
         image_params
     } else {
@@ -132,8 +51,8 @@ pub fn execute_linux_kernel_32(
     }
 
     let mut boot_params_raw = vec![0; 0x4000];
-    let boot_params: &mut boot_params =
-        unsafe { &mut *boot_params_raw.as_mut_ptr().cast::<boot_params>() };
+    let boot_params: &mut BootParams =
+        unsafe { &mut *boot_params_raw.as_mut_ptr().cast::<BootParams>() };
 
     unsafe {
         ptr::copy(&image_params.hdr, &mut boot_params.hdr, 1);
@@ -159,7 +78,7 @@ pub fn execute_linux_kernel_32(
     let start = boot_params.hdr.code32_start + image_params.hdr.handover_offset;
 
     type Handover =
-        unsafe extern "sysv64" fn(Handle, SystemTable<Boot>, *mut boot_params);
+        unsafe extern "sysv64" fn(Handle, SystemTable<Boot>, *mut BootParams);
 
     let start_ptr = start as *const ();
 
