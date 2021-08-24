2e2b7023f77d9fc426c37347204e86b71c844a04
Nicholas Bishop <nicholasbishop@google.com>
2021-08-24 17:54:52 -0400

    Use cargo workspaces
    Rather than having four entirely separate crates (tools, enroller,
    vboot, and crdyboot), use [cargo workspaces].
    
    This is primarily being done as a step toward using the [xtask] pattern,
    but it also fixes the issue with compiler message paths, allowing all
    the RUSTFLAGS hacks to be removed.
    
    A few notes:
    - Packages in a workspace share a single Cargo.lock file, so the
      per-crate ones have been removed.
    - The resolver setting goes in the root workspace crate, so moved that
      out of the per-crate Cargo.tomls.
    - Same for profiles, moved overflow-checks to the root Cargo.toml.
    - All build output now goes to the target directory in the workspace
      root, so updated a few paths for that.
    
    [cargo workspaces]: https://doc.rust-lang.org/cargo/reference/workspaces.html
    [xtask]: https://github.com/matklad/cargo-xtask/
    
    BUG=None
    TEST=./x.py check && ./x.py build-enroller && ./x.py update-disk
    
    Change-Id: Ie89be641deeda3b5140b2b4b233b954d0245e04b
    Reviewed-on: https://chromium-review.googlesource.com/c/crdyboot/+/3122929
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Esther Shimanovich <eshimanovich@google.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>

--- x.py
+++ x.py
@@ -8,21 +8,12 @@ import sys
 
 def main():
     repo_dir = os.path.dirname(os.path.realpath(__file__))
-    tools_dir = os.path.join(repo_dir, 'tools')
-    tools_manifest = os.path.join(tools_dir, 'Cargo.toml')
 
-    cmd = ['cargo', 'run', '--quiet', '--manifest-path', tools_manifest, '--']
+    cmd = ['cargo', 'run', '--quiet', '--package', 'crdyboot_tools', '--']
     cmd += ['--repo', repo_dir]
     cmd += sys.argv[1:]
 
-    # Make rustc use absolute paths for messages.
-    var_name = 'RUSTFLAGS'
-    env = dict(os.environ)
-    flags = env.get(var_name, '')
-    flags += ' --remap-path-prefix=src={}/src'.format(tools_dir)
-    env[var_name] = flags
-
-    res = subprocess.run(cmd, check=False, env=env)
+    res = subprocess.run(cmd, check=False)
     # Exit with the child's return code. Do it this way instead of
     # using check=True because we don't want a stack trace.
     if res.returncode != 0:
