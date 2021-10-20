88cd4d1b6b70753b82acfe262fa8f27f3bb373f8
Nicholas Bishop <nicholasbishop@google.com>
2021-10-20 19:00:32 -0400

    Add SBAT library
    This library is not yet used by anything.
    
    BUG=b:203705645
    TEST=cargo xtask check
    
    Change-Id: I99d755e3198e1f31e59a08c426cd1cf29d41d7ae

---
+++ sbat/src/generation.rs
@@ -0,0 +1,31 @@
+//! SBAT component generation.
+
+use crate::{Error, Result};
+use ascii::AsciiStr;
+use core::str::FromStr;
+
+/// SBAT component generation.
+///
+/// This is the machine-comparable version number of a component. It is
+/// always a positive integer.
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Generation(u32);
+
+impl Generation {
+    /// Create a `Generation` from a `u32`. An error is returned if the
+    /// input is zero.
+    pub fn new(val: u32) -> Result<Self> {
+        if val == 0 {
+            Err(Error::InvalidGeneration)
+        } else {
+            Ok(Self(val))
+        }
+    }
+
+    /// Parse an ASCII string as a `Generation`.
+    pub fn from_ascii(s: &AsciiStr) -> Result<Self> {
+        let val =
+            u32::from_str(s.as_str()).map_err(|_| Error::InvalidGeneration)?;
+        Self::new(val)
+    }
+}
---
+++ sbat/src/lib.rs
@@ -0,0 +1,90 @@
+//! UEFI SBAT (Secure Boot Advanced Targeting)
+//!
+//! SBAT is used to revoke insecure UEFI exectuables in a way that won't
+//! eat up the limited storage space available in the UEFI environment.
+//!
+//! There are two important sources of data:
+//! 1. The SBAT metadata associated with each image describes the
+//!    components in that image.
+//! 2. The SBAT revocation data stored in a UEFI variable provides a
+//!    list of component versions that are no longer allowed to boot.
+//!
+//! Each entry in the revocation list contains component name and
+//! version fields. (The first entry, which is the sbat version, also
+//! has a date field, but it is purely cosmetic.) When validating an
+//! image, each component in the image is checked against the revocation
+//! entries. If the name matches, and if the component's version is less
+//! than the version in the corresponding revocation entry, the
+//! component is considered revoked and the image will not pass
+//! validation.
+//!
+//! The details and exact validation rules are described further in the
+//! SBAT.md and SBAT.example.md files in the shim repo:
+//!
+//! - https://github.com/rhboot/shim/blob/e5bf2ba744731646749b605a322c353011f93c8e/SBAT.md
+//! - https://github.com/rhboot/shim/blob/e5bf2ba744731646749b605a322c353011f93c8e/SBAT.example.md
+
+#![warn(missing_docs)]
+// Turn off std, except when running tests.
+#![cfg_attr(not(test), no_std)]
+
+extern crate alloc;
+
+mod csv;
+mod generation;
+mod metadata;
+mod revocations;
+
+use ascii::AsciiStr;
+
+pub use generation::Generation;
+pub use metadata::MetadataReader;
+pub use revocations::RevocationsReader;
+pub use revocations::ValidationResult::{Allowed, Revoked};
+
+/// SBAT component. This is the machine-readable portion of SBAT that is
+/// actually used for revocation (other fields are human-readable and
+/// not used for comparisons).
+#[derive(Debug, Eq, PartialEq)]
+pub struct Component<'a> {
+    /// Component name.
+    pub name: &'a AsciiStr,
+
+    /// Component generation.
+    pub generation: Generation,
+}
+
+impl<'a> Component<'a> {
+    const NUM_FIELDS: usize = 2;
+
+    /// Create a `Component`.
+    pub fn new(name: &AsciiStr, generation: Generation) -> Component {
+        Component { name, generation }
+    }
+}
+
+/// SBAT errors.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Error {
+    /// CSV field exceeds the maximum size.
+    FieldTooLarge,
+
+    /// CSV field is not ASCII. According to the SBAT spec, all fields
+    /// must be ASCII.
+    InvalidAscii,
+
+    /// CSV field is not a valid `Generation` number.
+    InvalidGeneration,
+
+    /// CSV has more records than allowed.
+    TooManyRecords,
+
+    /// Parsed CSV exceeds the maximum allocation size.
+    TooMuchData,
+
+    /// CSV record has two few fields.
+    TooFewFields,
+}
+
+/// SBAT `Result` type alias.
+pub type Result<T> = core::result::Result<T, Error>;
