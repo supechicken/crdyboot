548850c3d04eb0c1d0cc5529aaf699c30162460c
Nicholas Bishop <nicholasbishop@google.com>
2022-05-06 16:18:01 -0400

    xtask: Remove some uses of loopback
    When updating bootloaders on the image (both shim and crdyboot), use
    `gptman` and `fatfs` to directly update the disk image rather than
    calling out to losetup and mount. This avoids some more uses of `sudo`.
    
    Delete the `mount` module entirely since it is no longer used.
    
    BUG=None
    TEST=cargo xtask check
    TEST=cargo xtask setup path/to/reven.bin
    TEST=cargo xtask update-disk
    TEST=cargo xtask qemu --secure-boot
    TEST=cargo xtask qemu --secure-boot --ia32
    
    Change-Id: Ic0e53da4c433217d668f035d742cc8ecdca46026

--- xtask/src/sign.rs
+++ xtask/src/sign.rs
@@ -1,4 +1,3 @@
-use crate::copy_file;
 use anyhow::Error;
 use camino::{Utf8Path, Utf8PathBuf};
 use command_run::Command;
@@ -113,40 +112,16 @@ fn convert_pem_to_der(input: &Utf8Path, output: &Utf8Path) {
     ]).run()?;
 }
 
+/// Sign the file at `src` using the keys provided by `key_paths`. The
+/// signed result is written to `dst` (and the `src` is never modified).
 #[throws]
-pub fn sign_all(efi: &Utf8Path, key_paths: &KeyPaths, file_names: &[String]) {
-    let tmp_dir = tempfile::tempdir()?;
-    let tmp_path = Utf8Path::from_path(tmp_dir.path()).unwrap();
-
-    let tmp_unsigned = tmp_path.join("unsigned.efi");
-    let tmp_signed = tmp_path.join("signed.efi");
-    let tmp_db = tmp_path.join("db");
-
-    fs::create_dir(&tmp_db)?;
-
-    for file_name in file_names {
-        let file_path = efi.join("efi/boot").join(file_name);
-        copy_file(&file_path, &tmp_unsigned)?;
-
-        #[rustfmt::skip]
-        Command::with_args("sbsign", &[
-            "--key", key_paths.priv_pem().as_str(),
-            "--cert", key_paths.pub_pem().as_str(),
-            tmp_unsigned.as_str(),
-            "--output", tmp_signed.as_str(),
-        ])
-        .run()?;
-
-        Command::with_args("sbverify", &["--list", tmp_signed.as_str()])
-            .run()?;
-
-        Command::with_args(
-            "sudo",
-            &["cp", tmp_signed.as_str(), file_path.as_str()],
-        )
-        .run()?;
-
-        fs::remove_file(&tmp_unsigned)?;
-        fs::remove_file(&tmp_signed)?;
-    }
+pub fn sign(src: &Utf8Path, dst: &Utf8Path, key_paths: &KeyPaths) {
+    #[rustfmt::skip]
+    Command::with_args("sbsign", &[
+        "--key", key_paths.priv_pem().as_str(),
+        "--cert", key_paths.pub_pem().as_str(),
+        src.as_str(),
+        "--output", dst.as_str(),
+    ])
+    .run()?;
 }
