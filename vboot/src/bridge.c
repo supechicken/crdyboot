7032a13a0686e5e6442e0df73b8631b414a9360c
Nicholas Bishop <nicholasbishop@google.com>
2022-06-24 17:05:54 -0400

    vboot: Add new entry point
    The new vb2api_init_ctx_for_kernel_verification_only function is a
    prototype for adding a new entry point to vboot_reference. The goal is
    to upstream this entry point into vboot_reference so that we can be sure
    our use case is tested as part of vboot_reference and continues to
    function as that repo changes.
    
    The new code is pretty much the C version of some existing Rust code in
    the `vboot` package. `validate_packed_pubkey_size` is no longer needed
    since that validation is now done in the entry point.
    
    BUG=b:237093169
    TEST=cargo xtask check
    TEST=cargo xtask update-disk
    TEST=cargo xtask qemu --secure-boot
    TEST=cargo xtask qemu --secure-boot --ia32
    
    Change-Id: I2913c8fbecc0480c3b269db6a85557b4104f3ad4
    Reviewed-on: https://chromium-review.googlesource.com/c/crdyboot/+/3721285
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Steve Kendall <skend@google.com>
    Commit-Queue: Steve Kendall <skend@google.com>
    Auto-Submit: Nicholas Bishop <nicholasbishop@google.com>

--- vboot/src/bridge.c
+++ vboot/src/bridge.c
@@ -2,18 +2,54 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <2common.h>
+
 #include "bindgen.h"
 
-// Update the shared data struct to point at the packed kernel key. Also
-// take care of calling vb2_set_workbuf_used.
-//
-// This code is written in C instead of Rust since it calls macros and
-// inline functions.
-void crdyboot_set_kernel_key(struct vb2_context *ctx,
-                             const struct vb2_packed_key *packed_key,
-                             const struct vb2_workbuf *wb) {
-  struct vb2_shared_data *sd = vb2_get_sd(ctx);
-  sd->kernel_key_offset = vb2_offset_of(sd, packed_key);
-  sd->kernel_key_size = packed_key->key_offset + packed_key->key_size;
-  vb2_set_workbuf_used(ctx, vb2_offset_of(sd, wb->buf));
+vb2_error_t vb2api_init_ctx_for_kernel_verification_only(
+    struct vb2_context *ctx, const uint8_t *kernel_packed_key_data,
+    uint32_t kernel_packed_key_data_size) {
+  /* TODO: add new VB2 error codes for the errors returned by this
+   * function. */
+
+  struct vb2_shared_data *sd;
+  struct vb2_workbuf wb;
+  struct vb2_packed_key *kernel_packed_key;
+  uint32_t kernel_packed_key_size;
+  void *dst_packed_key;
+
+  sd = vb2_get_sd(ctx);
+  vb2_workbuf_from_ctx(ctx, &wb);
+
+  /* Validate that the key data is at least as big as the header struct. */
+  if (kernel_packed_key_data_size < sizeof(struct vb2_packed_key)) {
+    return VB2_ERROR_UNPACK_KEY_SIZE;
+  }
+
+  /* Get the total size of the packed key data by inspecting the packed
+   * key header. Validate that the input key data is at least as large
+   * as the total size of the data. */
+  kernel_packed_key = (struct vb2_packed_key *)kernel_packed_key_data;
+  kernel_packed_key_size =
+      kernel_packed_key->key_offset + kernel_packed_key->key_size;
+  if (kernel_packed_key_size > kernel_packed_key_data_size) {
+    return VB2_ERROR_UNPACK_KEY_SIZE;
+  }
+
+  /* Allocate space in the workbuf to copy the packed key into. */
+  dst_packed_key = vb2_workbuf_alloc(&wb, kernel_packed_key_size);
+  if (!dst_packed_key) {
+    return VB2_ERROR_WORKBUF_SMALL;
+  }
+
+  /* Copy the packed key data into the workbuf. */
+  memcpy(dst_packed_key, kernel_packed_key_data, kernel_packed_key_size);
+
+  /* Set the location of the kernel key data in the context. */
+  sd->kernel_key_offset = vb2_offset_of(sd, dst_packed_key);
+  sd->kernel_key_size = kernel_packed_key_size;
+
+  vb2_set_workbuf_used(ctx, sd->kernel_key_offset + kernel_packed_key_size);
+
+  return VB2_SUCCESS;
 }
