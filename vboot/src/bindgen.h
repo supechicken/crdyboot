37a582650bd37b7f8f4b2424612ec2c240968746
Nicholas Bishop <nicholasbishop@google.com>
2022-09-07 15:41:29 -0400

    vboot: Cleanups in bindgen and build.rs
    * bindgen: remove some unnecessary functions and types.
    * bindgen header: drop the `vb2api_inject_kernel_subkey` declaration,
      this is part of vboot_reference now. Also drop an unnecessary unused
      include.
    * bridge library: re-enable some warnings that are no longer triggered.
    
    BUG=None
    TEST=cargo xtask check
    
    Change-Id: I42b15de9705256bdc30f107e7cd8fc92a752d0d3
    Reviewed-on: https://chromium-review.googlesource.com/c/crdyboot/+/3879686
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Commit-Queue: Jeffery Miller <jefferymiller@google.com>
    Reviewed-by: Jeffery Miller <jefferymiller@google.com>
    Auto-Submit: Nicholas Bishop <nicholasbishop@google.com>

--- vboot/src/bindgen.h
+++ vboot/src/bindgen.h
@@ -2,18 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This is the public API for vboot.
 #include <vb2_api.h>
 
+// For CROS_CONFIG_SIZE and CROS_PARAMS_SIZE constants.
 #include <futility/kernel_blob.h>
-#include <vboot_api.h>
-
-/**
- * TODO: proposed new entry point to be added to vboot_reference.
- *
- * This takes a context initialized with `vb2api_init` and a packed
- * kernel verification key. The packed key starts with a `struct
- * vb2_packed_key` header, followed by the actual key data.
- */
-vb2_error_t vb2api_inject_kernel_subkey(
-    struct vb2_context *ctx, const uint8_t *kernel_packed_key_data,
-    uint32_t kernel_packed_key_data_size);
