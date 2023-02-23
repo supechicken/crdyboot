a034ec4cd6a4cec9795468c3e78df0c3524a33ed
Nicholas Bishop <nicholasbishop@google.com>
2023-02-23 15:03:02 -0500

    xtask: Add successful boot test
    This is essentially the same as running `cargo xtask qemu` and verifying
    the VM starts successfully, but automated.
    
    BUG=b:238316146
    TEST=cargo xtask check --vm-test
    
    Change-Id: I6d1bd103137254c8b8ff3efe021204875208c82d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crdyboot/+/4288489
    Reviewed-by: Esther Shimanovich <eshimanovich@google.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>

--- xtask/src/vm_test.rs
+++ xtask/src/vm_test.rs
@@ -2,14 +2,112 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use crate::arch::Arch;
 use crate::config::Config;
 use crate::gen_disk::VerboseRuntimeLogs;
+use crate::qemu::{Display, QemuOpts};
 use crate::run_crdyboot_build;
-use anyhow::Result;
+use anyhow::{bail, Result};
+use camino::Utf8Path;
+use command_run::Command;
+use std::time::{Duration, Instant};
+use std::{env, thread};
+
+/// Make sure the SSH key is the expected test key.
+fn validate_test_key(key_path: &Utf8Path) -> Result<()> {
+    let output = Command::with_args(
+        "ssh-keygen",
+        [
+            // Show the fingerprint.
+            "-l",
+            // Set the file path.
+            "-f",
+            key_path.as_str(),
+        ],
+    )
+    .enable_capture()
+    .run()?;
+    let stdout = String::from_utf8(output.stdout)?;
+    let expected = "Fp1qWjFLyK1cTpiI5rdk7iEJwoK9lcnYAgbQtGC3jzU";
+    if !stdout.contains(expected) {
+        bail!("incorrect key; expected {expected}, got {}", stdout.trim());
+    }
+    Ok(())
+}
+
+/// Wait for SSH to come up on the VM (indicating a successful
+/// boot). Times out after one minute.
+fn wait_for_ssh() -> Result<()> {
+    // Get the standard testing key. Fail early with an error if it
+    // doesn't exist.
+    let identity_path = Utf8Path::new(&env::var("HOME").unwrap()).join(".ssh/testing_rsa");
+    if let Err(err) = validate_test_key(&identity_path) {
+        bail!("missing or invalid test key in {identity_path}; copy it from https://chromium.googlesource.com/chromiumos/chromite/+/HEAD/ssh_keys/testing_rsa and set the permissions with `chmod 600` ({err})");
+    }
+
+    println!("waiting for SSH");
+    let mut cmd = Command::with_args(
+        "ssh",
+        [
+            "-oStrictHostKeyChecking=no",
+            "-oUserKnownHostsFile=/dev/null",
+            // Enable batch mode to prevent password prompts if the SSH
+            // key is misconfigured.
+            "-oBatchMode=yes",
+            "-i",
+            identity_path.as_str(),
+            "-p",
+            &Config::ssh_port().to_string(),
+            "root@localhost",
+            "true",
+        ],
+    );
+    cmd.check = false;
+    cmd.capture = true;
+    cmd.log_command = false;
+
+    // Wait up to one minute for SSH to come up.
+    let start_time = Instant::now();
+    while start_time.elapsed() < Duration::from_secs(60) {
+        let output = cmd.run()?;
+        if output.status.success() {
+            return Ok(());
+        }
+
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    bail!("SSH didn't come up");
+}
+
+/// Test successful boots on both ia32 and x64.
+fn test_successful_boot(conf: &Config) -> Result<()> {
+    for arch in Arch::all() {
+        println!("test successful boot with arch={arch:?}");
+        let opts = QemuOpts {
+            capture_output: true,
+            display: Display::None,
+            // No need to copy the disk for this test since we aren't
+            // modifying it.
+            image_path: conf.disk_path().to_path_buf(),
+            ovmf: conf.ovmf_paths(arch),
+            secure_boot: true,
+            timeout: None,
+            tpm_version: None,
+        };
+        let _vm = opts.spawn_disk_image(conf)?;
+
+        // Check that SSH comes up, indicating a successful boot.
+        wait_for_ssh()?;
+    }
+
+    Ok(())
+}
 
 pub fn run_vm_tests(conf: &Config) -> Result<()> {
-    // Ensure the build is up-to-date.
     run_crdyboot_build(conf, VerboseRuntimeLogs(true))?;
 
-    todo!("add VM tests here")
+    test_successful_boot(conf)?;
+
+    Ok(())
 }
