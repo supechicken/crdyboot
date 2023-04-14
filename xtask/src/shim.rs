55282ff143ee561028468fa2c18f255d0ee73df0
Nicholas Bishop <nicholasbishop@google.com>
2023-04-14 17:17:08 -0400

    xtask: Update to latest commit in shim-review
    This version uses crdyboot by default, so the Dockerfile modifications
    are no longer needed.
    
    BUG=None
    TEST=cargo xtask setup
    TEST=cargo xtask check
    TEST=cargo xtask qemu
    
    Change-Id: If518bc17ec1273fc5b76832f72aa49adcd6fc662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crdyboot/+/4427206
    Auto-Submit: Nicholas Bishop <nicholasbishop@google.com>
    Commit-Queue: Jeffery Miller <jefferymiller@google.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Jeffery Miller <jefferymiller@google.com>

--- xtask/src/shim.rs
+++ xtask/src/shim.rs
@@ -7,19 +7,16 @@ use crate::config::Config;
 use crate::{copy_file, gen_disk};
 use anyhow::Result;
 use command_run::Command;
-use fs_err as fs;
 use gen_disk::SignAndUpdateBootloader;
 
 fn build_shim(conf: &Config) -> Result<()> {
     let shim_dir = conf.shim_build_path();
     let shim_url = "https://chromium.googlesource.com/chromiumos/shim-review";
-    let shim_rev = "6d201a645d8b5169ea844ddafa7ba7659c3f356c";
+    let shim_rev = "f7a6edc0b6726497dc8b0badb70c3e6fa16590c0";
 
     if shim_dir.exists() {
-        // Remove local modifications so that the Dockerfile
-        // modification below doesn't keep inserting the same change,
-        // and so that the checked-out revision can be changed without
-        // conflicts.
+        // Remove local modifications so that the checked-out revision
+        // can be changed without conflicts.
         Command::with_args("git", ["-C", shim_dir.as_str(), "checkout", "-f"]).run()?;
     }
 
@@ -29,36 +26,6 @@ fn build_shim(conf: &Config) -> Result<()> {
         conf.secure_boot_shim_key_paths().pub_der(),
         shim_dir.join("chromeos_reven.cer"),
     )?;
-
-    // Apply some modifications to the Dockerfile:
-    //
-    // Disable EBS protection. This is a shim feature that hooks the
-    // ExitBootServices function so that shim can verify that the
-    // 2nd-stage bootloader properly used shim's verification protocol
-    // to check the signature of the next stage. In our case though,
-    // we verify the signature of the entire kernel partition through
-    // a different mechanism than what shim provides, so the EBS check
-    // would fail if enabled.
-    //
-    // Change the second-stage bootloader name from grub to crdyboot.
-    let dockerfile_path = shim_dir.join("Dockerfile");
-    let dockerfile = fs::read_to_string(&dockerfile_path)?;
-
-    let orig_str = "TOPDIR=.. -f ../Makefile";
-    let new_str = format!("DISABLE_EBS_PROTECTION=y {orig_str}");
-    let dockerfile = dockerfile.replace(orig_str, &new_str);
-
-    let default_loader_prefix = r"DEFAULT_LOADER=\\\\crdyboot";
-
-    let orig_str = "ARCH=x86_64";
-    let new_str = format!("{default_loader_prefix}x64.efi {orig_str}");
-    let dockerfile = dockerfile.replace(orig_str, &new_str);
-
-    let orig_str = "ARCH=ia32";
-    let new_str = format!("{default_loader_prefix}ia32.efi {orig_str}");
-    let dockerfile = dockerfile.replace(orig_str, &new_str);
-
-    fs::write(&dockerfile_path, dockerfile)?;
 
     Command::with_args("make", ["build"])
         .set_dir(&shim_dir)
