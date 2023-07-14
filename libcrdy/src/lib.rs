ffc4c93f88a43b4c5a92488a116f4a29353e59c4
Nicholas Bishop <nicholasbishop@google.com>
2023-07-14 14:00:11 -0400

    libcrdy: Deny clippy::arithmetic_side_effects
    This helps ensure that we properly handle arithmetic overflow by using
    checked arithmetic.
    
    BUG=b:291288022
    TEST=cargo xtask check --vm-tests
    
    Change-Id: Ia22dcbeac28f6608d7986e8b0b149cd849858a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crdyboot/+/4685643
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Jeffery Miller <jefferymiller@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>

--- libcrdy/src/lib.rs
+++ libcrdy/src/lib.rs
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#![deny(clippy::arithmetic_side_effects)]
 #![deny(clippy::pedantic)]
 #![allow(clippy::enum_glob_use)]
 #![allow(clippy::missing_errors_doc)]
