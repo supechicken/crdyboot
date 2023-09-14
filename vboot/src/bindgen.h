eb00fb71ebcf4883ff57f051f136afc28de4913c
Nicholas Bishop <nicholasbishop@google.com>
2023-09-14 14:44:21 -0400

    vboot: Fix cpplint lints
    Also fix a typo in the copyright header.
    
    BUG=none
    TEST=cargo xtask check
    TEST=cros lint .
    
    Change-Id: I47a5f7c4807ddf9410979c7e2a5aa9d88346b83e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crdyboot/+/4867072
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Auto-Submit: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Ted Brandston <tbrandston@google.com>
    Commit-Queue: Ted Brandston <tbrandston@google.com>

--- vboot/src/bindgen.h
+++ vboot/src/bindgen.h
@@ -2,8 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef VBOOT_SRC_BINDGEN_H_
+#define VBOOT_SRC_BINDGEN_H_
+
 // This is the public API for vboot.
 #include <vb2_api.h>
 
 // For CROS_CONFIG_SIZE and CROS_PARAMS_SIZE constants.
 #include <futility/kernel_blob.h>
+
+#endif  // VBOOT_SRC_BINDGEN_H_
--- vboot/src/libc/string.h
+++ vboot/src/libc/string.h
@@ -1,4 +1,4 @@
-// Copyright 2022 The ChromiumOS Author
+// Copyright 2022 The ChromiumOS Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,6 @@ int memcmp(const void *, const void *, size_t);
 void *memcpy(void *, const void *, size_t);
 void *memmove(void *, const void *, size_t);
 void *memset(void *, int, size_t);
-char *strcpy(char *dest, const char *src);
+char *strcpy(char *dest, const char *src);  // NOLINT
 
 #endif  // VBOOT_SRC_LIBC_STRING_H_
