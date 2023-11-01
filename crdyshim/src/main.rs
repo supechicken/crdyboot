1a2f1e917674981306b36fcac762e0f2b22aa251
Nicholas Bishop <nicholasbishop@google.com>
2023-11-01 13:15:21 -0400

    crdyshim: Add function to check secure boot status
    This function is not called yet, but will be used to allow booting an
    unsigned second-stage bootloader if secure boot is off. This behavior
    matches shim, and is important for development and testing of unsigned
    builds.
    
    BUG=b:203705645
    TEST=cargo xtask check
    
    Change-Id: Id507cad9265107142a343f9492b329d5b701e053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crdyboot/+/4995167
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>
    Tested-by: Nicholas Bishop <nicholasbishop@google.com>
    Reviewed-by: Ted Brandston <tbrandston@google.com>

--- crdyshim/src/main.rs
+++ crdyshim/src/main.rs
@@ -19,10 +19,48 @@ mod relocation;
 mod sbat_revocation;
 
 use libcrdy::embed_section;
+use log::info;
 use uefi::prelude::*;
+use uefi::table::runtime::VariableVendor;
 
 #[cfg(not(target_os = "uefi"))]
 use libcrdy::uefi_services;
+
+#[allow(clippy::doc_markdown)]
+/// Check whether secure boot is enabled or not.
+///
+/// The firmware communicates secure boot status with a global
+/// "SecureBoot" UEFI variable containing a `u8` value. If the value is
+/// 0, secure boot is disabled. If the value is 1, secure boot is
+/// enabled.
+///
+/// If the variable cannot be read, or if the value is anything other
+/// than 0 or 1, log an error and treat it as secure boot being
+/// disabled.
+fn is_secure_boot_enabled(runtime_services: &RuntimeServices) -> bool {
+    let mut buf: [u8; 1] = [0];
+    match runtime_services.get_variable(
+        cstr16!("SecureBoot"),
+        &VariableVendor::GLOBAL_VARIABLE,
+        &mut buf,
+    ) {
+        Ok(([0], _)) => false,
+        Ok(([1], _)) => true,
+        Ok((val, _)) => {
+            // Only the values 0 and 1 are valid per the spec. If the
+            // variable contains some other number, treat it as secure
+            // boot being disabled.
+            info!("unexpected SecureBoot value: {val:x?}");
+            false
+        }
+        Err(err) => {
+            // If the variable cannot be read, treat it as secure boot
+            // being disabled.
+            info!("failed to read SecureBoot variable: {}", err.status());
+            false
+        }
+    }
+}
 
 #[entry]
 fn efi_main(image: Handle, mut st: SystemTable<Boot>) -> Status {
