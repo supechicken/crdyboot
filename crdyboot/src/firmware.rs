79910501525d80f3df689f6b8d3609375296c513
Nicholas Bishop <nicholasbishop@google.com>
2024-08-14 11:44:56 -0400

    crdyboot: Add firmware::update_info module
    Move the UpdateInfo type and unit test into a new module.
    
    This is the first of three commits, with the end goal of making the
    UpdateInfo fields private (so only the `update_info` module will have
    access).
    
    BUG=b:338423918
    TEST=cargo xtask check
    
    Change-Id: Id8a81f5a38b3cdae55ef052ce3a140dc4f5c5d21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crdyboot/+/5783171
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>
    Reviewed-by: Ted Brandston <tbrandston@google.com>
    Commit-Queue: Nicholas Bishop <nicholasbishop@google.com>

--- crdyboot/src/firmware.rs
+++ crdyboot/src/firmware.rs
@@ -5,22 +5,19 @@
 #![allow(clippy::indexing_slicing)]
 
 mod load_capsules;
+mod update_info;
 
 use crate::disk::GptDiskError;
 use alloc::borrow::ToOwned;
-use alloc::boxed::Box;
-use alloc::string::String;
 use alloc::vec::Vec;
 use core::fmt::{self, Display, Formatter};
-use core::ops::Range;
-use core::{mem, ptr, slice};
-use ext4_view::{Ext4Error, PathBuf, PathError};
+use ext4_view::{Ext4Error, PathError};
 use load_capsules::load_capsules_from_disk;
 use log::{error, info, warn};
 use uefi::prelude::*;
-use uefi::proto::device_path::{DevicePath, DevicePathNodeEnum};
-use uefi::table::runtime::{Time, VariableAttributes, VariableKey, VariableVendor};
+use uefi::table::runtime::{VariableKey, VariableVendor};
 use uefi::{guid, CStr16, CString16, Status};
+use update_info::UpdateInfo;
 
 const FWUPDATE_ATTEMPT_UPDATE: u32 = 0x0000_0001;
 const FWUPDATE_ATTEMPTED: u32 = 0x0000_0002;
@@ -72,132 +69,6 @@ impl Display for FirmwareError {
             Self::Ext4ReadFailed(err) => write!(f, "failed to read an update capsule: {err}"),
         }
     }
-}
-
-/// Info for a single capsule update, provided by fwupd's [uefi-capsule]
-/// plugin via a UEFI variable.
-///
-/// [uefi-capsule]: https://github.com/fwupd/fwupd/tree/main/plugins/uefi-capsule
-#[derive(Debug, Eq, PartialEq)]
-struct UpdateInfo {
-    /// Name of the update's UEFI variable.
-    name: CString16,
-
-    /// Attributes of the update's UEFI variable.
-    attrs: VariableAttributes,
-
-    /// Raw data from the variable.
-    data: Box<[u8]>,
-}
-
-impl UpdateInfo {
-    /// Byte range of the `time_attempted` field.
-    const TIME_ATTEMPTED_RANGE: Range<usize> = 32..48;
-
-    /// Byte range of the `status` field.
-    const STATUS_RANGE: Range<usize> = 48..52;
-
-    /// Size of the fixed fields (plus padding) when in serialized
-    /// form. The rest of the data is a variable-length device path.
-    const HEADER_SIZE_IN_BYTES: usize = 52;
-
-    /// Create an `UpdateInfo` from a variable's name, attributes, and
-    /// data. Some minimal validation is performed.
-    fn new(
-        name: CString16,
-        attrs: VariableAttributes,
-        data: Box<[u8]>,
-    ) -> Result<Self, FirmwareError> {
-        // Return an error if there's not enough data.
-        if data.len() < UpdateInfo::HEADER_SIZE_IN_BYTES {
-            return Err(FirmwareError::UpdateInfoTooShort);
-        }
-
-        // Return an error if the device path is not valid.
-        <&DevicePath>::try_from(&data[Self::HEADER_SIZE_IN_BYTES..])
-            .map_err(|_| FirmwareError::UpdateInfoMalformedDevicePath)?;
-
-        Ok(Self { name, attrs, data })
-    }
-
-    /// Set the `time_attempted` field to the current time.
-    ///
-    /// If the current time cannot be retrieved, log an error and leave
-    /// the `time_attempted` field unchanged.
-    fn update_time_attempted(&mut self, rt: &RuntimeServices) {
-        // Get the current time.
-        let time: Time = match rt.get_time() {
-            Ok(time) => time,
-            Err(err) => {
-                warn!("failed to get current time: {err}");
-                return;
-            }
-        };
-
-        self.data[Self::TIME_ATTEMPTED_RANGE].copy_from_slice(time_to_bytes(&time));
-    }
-
-    fn status(&self) -> u32 {
-        u32::from_le_bytes(self.data[Self::STATUS_RANGE].try_into().unwrap())
-    }
-
-    fn set_status(&mut self, status: u32) {
-        self.data[Self::STATUS_RANGE].copy_from_slice(&status.to_le_bytes());
-    }
-
-    fn device_path(&self) -> &DevicePath {
-        let path = <&DevicePath>::try_from(&self.data[Self::HEADER_SIZE_IN_BYTES..]);
-        // OK to unwrap: the validity of the device path was checked in
-        // `UpdateInfo::new`.
-        path.unwrap()
-    }
-
-    /// Get the file path of the update capsule.
-    ///
-    /// The path is extracted from the final node of the device
-    /// path. The path is converted from a Windows-style path to a
-    /// Unix-style path, and made relative instead of absolute.
-    fn file_path(&self) -> Result<PathBuf, FirmwareError> {
-        let device_path = self.device_path();
-
-        // Get the last node, which should be a file path node.
-        let Some(DevicePathNodeEnum::MediaFilePath(path)) = device_path
-            .node_iter()
-            .last()
-            .and_then(|node| node.as_enum().ok())
-        else {
-            return Err(FirmwareError::FilePathMissing);
-        };
-
-        // The file path node contains an unaligned UCS-2 string.
-        // Convert it to a `CString16`.
-        let path = path
-            .path_name()
-            .to_cstring16()
-            .map_err(|_| FirmwareError::FilePathEncodingInvalid)?;
-
-        // Convert from UCS-2 to UTF-8.
-        let path = String::from(&path);
-
-        // Convert path separator style from Windows to Unix.
-        let path = path.replace('\\', "/");
-
-        // Make the path relative instead of absolute.
-        let path = path.trim_start_matches('/');
-
-        PathBuf::try_from(path).map_err(FirmwareError::FilePathInvalid)
-    }
-}
-
-// TODO(nicholasbishop): add a variation of this function to uefi-rs.
-fn time_to_bytes(time: &Time) -> &[u8] {
-    let time_ptr: *const Time = ptr::from_ref(time);
-    let byte_ptr: *const u8 = time_ptr.cast();
-    let num_bytes = mem::size_of::<Time>();
-
-    // SAFETY: `time` contains 16 bytes of valid data. It is correctly
-    // aligned since u8 has an alignment of 1.
-    unsafe { slice::from_raw_parts(byte_ptr, num_bytes) }
 }
 
 /// Get a list of all available updates by iterating through all UEFI
@@ -318,64 +189,4 @@ pub fn update_firmware(st: &SystemTable<Boot>) -> Result<(), FirmwareError> {
     set_update_statuses(st, &updates)
 
     // TODO(b/338423918): Apply the update capsules and reboot.
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use uefi::proto::device_path::build::{self, DevicePathBuilder};
-    use uefi::proto::device_path::media::{PartitionFormat, PartitionSignature};
-
-    #[test]
-    fn test_update_info() {
-        // This test file is a direct copy of an efivarfs file created
-        // by `fwupd install`.
-        let data = include_bytes!(
-            "../test_data/\
-            fwupd-61b65ccc-0116-4b62-80ed-ec5f089ae523-0-0abba7dc-e516-4167-bbf5-4d9d1c739416"
-        );
-        let name = cstr16!("fwupd-61b65ccc-0116-4b62-80ed-ec5f089ae523-0").to_owned();
-        // Efivarfs stores the UEFI variable attributes in the first
-        // four bytes.
-        let attrs = VariableAttributes::from_bits_retain(u32::from_le_bytes(
-            data[0..4].try_into().unwrap(),
-        ));
-        let data = &data[4..];
-
-        let mut info = UpdateInfo::new(name, attrs, data.to_vec().into_boxed_slice()).unwrap();
-
-        // Create the expected device path.
-        let mut storage = Vec::new();
-        let expected_path = DevicePathBuilder::with_vec(&mut storage)
-            .push(&build::media::HardDrive {
-                partition_number: 12,
-                partition_start: 0,
-                partition_size: 0,
-                partition_signature: PartitionSignature::Guid(guid!(
-                    "99cc6f39-2fd1-4d85-b15a-543e7b023a1f"
-                )),
-                partition_format: PartitionFormat::GPT,
-            })
-            .unwrap()
-            .push(&build::media::FilePath {
-                path_name: cstr16!(
-                    r"\EFI\chromeos\fw\fwupd-61b65ccc-0116-4b62-80ed-ec5f089ae523.cap"
-                ),
-            })
-            .unwrap()
-            .finalize()
-            .unwrap();
-
-        assert_eq!(info.device_path(), expected_path);
-        assert_eq!(info.status(), FWUPDATE_ATTEMPT_UPDATE);
-
-        // Check setting the status.
-        info.set_status(123);
-        assert_eq!(info.status(), 123);
-
-        assert_eq!(
-            info.file_path().unwrap(),
-            "EFI/chromeos/fw/fwupd-61b65ccc-0116-4b62-80ed-ec5f089ae523.cap"
-        );
-    }
 }
