7abeb2208d52c5a680291b667bff80a20aedfa96
Jeffery Miller <jefferymiller@google.com>
2024-09-06 09:01:25 -0500

    crdyboot: avb: Load kernel from avb partition
    Copy the kernel from the verified partition into memory
    allocated to execute the kernel.
    
    BUG=b:365802655
    TEST=cargo xtask check
    TEST=cargo xtask build --android
    TEST=copy to ESP of an android image; boot with qemu
    
    Change-Id: I9d1339f49a75e0a8cbf947a849ebd39b7b3f63de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crdyboot/+/5856106
    Tested-by: Jeffery Miller <jefferymiller@google.com>
    Reviewed-by: Nicholas Bishop <nicholasbishop@google.com>
    Commit-Queue: Jeffery Miller <jefferymiller@google.com>
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>

--- crdyboot/src/avb.rs
+++ crdyboot/src/avb.rs
@@ -8,11 +8,13 @@ use avb::avb_sys::{
     avb_slot_verify, avb_slot_verify_result_to_string, AvbHashtreeErrorMode, AvbIOResult,
     AvbPartitionData, AvbSlotVerifyData, AvbSlotVerifyFlags, AvbSlotVerifyResult, AvbVBMetaData,
 };
+use bootimg::BootImage;
 use core::ffi::CStr;
 use core::{ptr, slice, str};
-use libcrdy::page_alloc::ScopedPageAllocation;
+use libcrdy::page_alloc::{PageAllocationError, ScopedPageAllocation};
 use libcrdy::uefi::UefiImpl;
 use log::{debug, log_enabled};
+use uefi::table::boot::{AllocateType, MemoryType};
 use uefi::CString16;
 
 /// Allocated buffers from AVB to execute the kernel.
@@ -31,6 +33,30 @@ pub enum AvbError {
     /// Missing a required avb partition.
     #[error("missing the required partition: {0}")]
     MissingAvbPartition(&'static str),
+
+    /// Failed to allocate a buffer.
+    #[error("failed to allocate memory")]
+    Allocation(#[source] PageAllocationError),
+
+    /// Failed to parse bootimage partition header.
+    #[error("unable to parse bootimage partition header: {0}")]
+    BootImageHeaderParseError(bootimg::ImageError),
+
+    /// Parsed a bootimg partition header of the wrong version.
+    #[error("the partition header is not V4")]
+    UnsupportedPartitionHeaderVersion,
+
+    /// Invalid kernel size.
+    #[error("invalid kernel size: {0}")]
+    InvalidKernelSize(u32),
+
+    /// Invalid ramdisk size.
+    #[error("invalid ramdisk size: {0}")]
+    InvalidRamdiskSize(u32),
+
+    /// Index is out of bounds for the avb image header.
+    #[error("index is out of bounds of the avb image header: {0}")]
+    IndexOutOfBounds(&'static str),
 }
 
 fn verify_result_to_str(r: AvbSlotVerifyResult) -> &'static str {
@@ -40,6 +66,9 @@ fn verify_result_to_str(r: AvbSlotVerifyResult) -> &'static str {
     // Unwrap is OK, the string is always UTF-8.
     s.to_str().unwrap()
 }
+
+// BootImage boot header page size.
+const BOOT_HEADER_PAGE_SIZE: usize = 4096;
 
 pub struct AvbDiskOpsImpl;
 
@@ -109,6 +138,108 @@ impl AvbDiskOps for AvbDiskOpsImpl {
         dest.copy_from_slice(&guid.to_ascii_hex_lower());
         Ok(())
     }
+}
+
+/// Slices to the data sections in a `BootImage` header.
+/// Only the sections that relevant for this bootloader
+/// are included.
+struct BootImageParts<'a> {
+    kernel: &'a [u8],
+    #[expect(dead_code)]
+    initramfs: &'a [u8],
+}
+
+impl<'a> BootImageParts<'a> {
+    /// Determine the section slices for a partition with a
+    /// `BootImage` header.
+    fn from_avb_boot_partition(
+        partition: &AvbPartitionData,
+    ) -> Result<BootImageParts<'a>, AvbError> {
+        let data = unsafe { slice::from_raw_parts(partition.data, partition.data_size) };
+        let header = BootImage::parse(data).map_err(AvbError::BootImageHeaderParseError)?;
+        let BootImage::V4(header) = header else {
+            return Err(AvbError::UnsupportedPartitionHeaderVersion);
+        };
+
+        // From [bootimg.h boot header v4]
+        // When the boot image header has a version of 4, the structure of the boot
+        // image is as follows:
+        //
+        // +---------------------+
+        // | boot header         | 4096 bytes
+        // +---------------------+
+        // | kernel              | m pages
+        // +---------------------+
+        // | ramdisk             | n pages
+        // +---------------------+
+        // | boot signature      | g pages
+        // +---------------------+
+        //
+        // m = (kernel_size + 4096 - 1) / 4096
+        // n = (ramdisk_size + 4096 - 1) / 4096
+        // g = (signature_size + 4096 - 1) / 4096
+        //
+        // Here the page size is fixed at 4096 bytes.
+        // [bootimg.h boot header v4]: https://android.googlesource.com/platform/system/tools/mkbootimg/+/refs/heads/main/include/bootimg/bootimg.h#324
+
+        let base = header._base;
+
+        let kernel_size = usize::try_from(base.kernel_size)
+            .map_err(|_| AvbError::InvalidKernelSize(base.kernel_size))?;
+
+        // The kernel slice starts after the boot header at one page(4096).
+        let kernel_offset = BOOT_HEADER_PAGE_SIZE;
+
+        let ramdisk_size = usize::try_from(base.ramdisk_size)
+            .map_err(|_| AvbError::InvalidRamdiskSize(base.ramdisk_size))?;
+
+        // Offset to the start of the ramdisk which will fall on
+        // a page size boundary.
+        let ramdisk_offset = kernel_size
+            .checked_next_multiple_of(BOOT_HEADER_PAGE_SIZE)
+            .and_then(|x| x.checked_add(kernel_offset))
+            .ok_or(AvbError::IndexOutOfBounds("ramdisk"))?;
+
+        // Get a slice pointing to the kernel's data.
+        let kernel = kernel_offset
+            .checked_add(kernel_size)
+            .and_then(|x| data.get(kernel_offset..x))
+            .ok_or(AvbError::IndexOutOfBounds("kernel"))?;
+
+        // Get a slice pointing to the ramdisk data.
+        let initramfs = ramdisk_offset
+            .checked_add(ramdisk_size)
+            .and_then(|x| data.get(ramdisk_offset..x))
+            .ok_or(AvbError::IndexOutOfBounds("ramdisk"))?;
+
+        Ok(BootImageParts { kernel, initramfs })
+    }
+}
+
+fn load_kernel(boot_part: &AvbPartitionData) -> Result<ScopedPageAllocation, AvbError> {
+    // From the "boot" partition only the kernel is used.
+    let kernel_src = BootImageParts::from_avb_boot_partition(boot_part)?.kernel;
+
+    // Kernel must be non-zero.
+    if kernel_src.is_empty() {
+        return Err(AvbError::InvalidKernelSize(0));
+    }
+    // Allocate a buffer that can boot the kernel.
+    let mut kernel_buffer = ScopedPageAllocation::new_unaligned(
+        AllocateType::AnyPages,
+        MemoryType::LOADER_CODE,
+        kernel_src.len(),
+    )
+    .map_err(AvbError::Allocation)?;
+    // Any padding will stay 0 from the page allocation.
+    // unwrap safe: The allocated buffer is larger than the kernel_size.
+    kernel_buffer
+        .get_mut(..kernel_src.len())
+        .unwrap()
+        .copy_from_slice(kernel_src);
+    // TODO: pass back the actual kernel length? The callers
+    // might find this useful (to measure for example).
+    Ok(kernel_buffer)
 }
 
 fn debug_print_avb_vbmeta_data(verify_data: *const AvbSlotVerifyData) {
@@ -207,7 +338,7 @@ pub fn do_avb_verify() -> Result<LoadedBuffersAvb, AvbError> {
         }
     }
 
-    let Some(_boot) = boot else {
+    let Some(boot) = boot else {
         return Err(AvbError::MissingAvbPartition("boot"));
     };
     let Some(_vendor_boot) = vendor_boot else {
@@ -216,6 +347,9 @@ pub fn do_avb_verify() -> Result<LoadedBuffersAvb, AvbError> {
     let Some(_init_boot) = init_boot else {
         return Err(AvbError::MissingAvbPartition("init_boot"));
     };
+
+    // Load the kernel buffer from the boot partition header.
+    let _kernel_buffer = load_kernel(boot)?;
 
     todo!("allocate, load and return buffers");
 }
