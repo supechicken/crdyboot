03f05df8bee462099bdb559495bd06f69ed122a4
Jeffery Miller <jefferymiller@google.com>
2024-11-25 10:38:05 -0600

    avb: Use the buildutil provided Target
    Use the buildutil workspace provided Target and helper
    scripts.
    
    BUG=None
    TEST=cargo build --package avb --target x86_64-unknown-uefi
    TEST=cargo build --package avb
    
    Change-Id: I33446cc94a4dd8fc719d5c5b339cb8ac3a1f475e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crdyboot/+/6048917
    Tested-by: Jeffery Miller <jefferymiller@google.com>
    Reviewed-by: Nicholas Bishop <nicholasbishop@google.com>
    Commit-Queue: Jeffery Miller <jefferymiller@google.com>
    Tested-by: chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com <chromeos-cop-builder@chromeos-cop.iam.gserviceaccount.com>

--- avb/build.rs
+++ avb/build.rs
@@ -2,75 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO: This file is mostly a copy of vboot/build.rs
-// and contains quite a bit of duplicate code.
+use buildutil::{path_to_str, rerun_if_changed, Target};
 use std::path::{Path, PathBuf};
 use std::{env, fs, process};
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum Target {
-    UefiI686,
-    UefiX86_64,
-    Host,
-}
-
-impl Target {
-    /// Read the target from the `TARGET` env var.
-    fn from_env() -> Target {
-        let target = env::var("TARGET").unwrap();
-        match target.as_str() {
-            "i686-unknown-uefi" => Self::UefiI686,
-            "x86_64-unknown-uefi" => Self::UefiX86_64,
-            // For everything else, assume it's a host build
-            // (e.g. "cargo test").
-            _ => Self::Host,
-        }
-    }
-
-    /// True if this is a UEFI target, false if it's a host target.
-    fn is_uefi(self) -> bool {
-        match self {
-            Self::UefiI686 | Self::UefiX86_64 => true,
-            Self::Host => false,
-        }
-    }
-
-    /// Get a target triple to override the default C compiler
-    /// target. Returns None if this is a host build so that the default
-    /// target is used in that case.
-    ///
-    /// The targets chosen here match those in the `cc` crate:
-    /// https://github.com/rust-lang/cc-rs/pull/623/files
-    fn c_target_override(self) -> Option<&'static str> {
-        match self {
-            Self::UefiI686 => Some("i686-unknown-windows-gnu"),
-            Self::UefiX86_64 => Some("x86_64-unknown-windows-gnu"),
-            Self::Host => None,
-        }
-    }
-
-    fn avb_build_subdir(self) -> &'static str {
-        self.c_target_override().unwrap_or("host")
-    }
-}
-
-/// Convert a `Path` to a `str`, or panic if the path isn't UTF-8.
-fn path_to_str(path: &Path) -> &str {
-    if let Some(s) = path.to_str() {
-        s
-    } else {
-        panic!("{} is not a UTF-8 path", path.display());
-    }
-}
-
-fn rerun_if_changed<P: AsRef<Path>>(path: P) {
-    println!("cargo:rerun-if-changed={}", path_to_str(path.as_ref()));
-}
 
 /// Build avb verify lib
 fn build_avb_lib(avb_ref: &Path, target: Target, c_compiler: &str) {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let avb_build_dir = out_dir.join("avb_build").join(target.avb_build_subdir());
+    let avb_build_dir = out_dir.join("avb_build").join(target.build_subdir());
 
     // Delete the output directory if it already exists, to ensure a
     // clean build. This is useful because makefiles are never 100%
